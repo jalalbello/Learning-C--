d686179ec79a21950e5eb98601232a4638fea63e
jalalbello <jalaleddinebellouchi@gmail.com>
2022-10-14 11:29:11 +0100

    Push Print Lesson

---
+++ C++ Output (Print Text)/PrintProgram.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    // The single quote is used to identify the single charachter, and double quotes are used for string literals
+
+    // std::cout << 'Hello World'; this code does not work 
+
+    std::cout << "Hello World"<< std::endl; // double quotes work, std:end1 to add ne wline at the end
+    std::cout << "I am Learning C++" ; // double quotes work
+
+    // \n for new line
+
+    std::cout << "\nI love learning"; // this creates a  new line before and after
+    
+    return 0;
+}
---
+++ C++ Output (Print Text)/notes.md
@@ -0,0 +1,11 @@
+## C++ Output (Print Text)
+
+The cout object, together with the << operator, is used to output values/print text:
+
+
+| Escape Sequence |                       Description |
+| :-------------- | --------------------------------: |
+| \t              |          Creates a horizontal tab |
+| \\              | Inserts a backslash character (\) |
+| \"              |  Inserts a double quote character |
+| \n              |                Inserts a new line |
